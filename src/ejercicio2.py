b89ed3c414e5494ad3438038e9a6353c9e15e398
AlejandroFdezGarcia <101207228+AlejandroFdezGarcia@users.noreply.github.com>
2023-03-23 11:54:08 +0100

    Update ejercicio2.py

--- src/ejercicio2.py
+++ src/ejercicio2.py
@@ -1,1 +1,7 @@
 # EJERCICIO 2
+import sqlite3
+con = sqlite3.connect('practica1.db')
+cur = con.cursor()
+cur.execute("CREATE TABLE maquinas (id text, ip text, localizacion text, responsablenombre text, responsabletelefono integer, responsablerol text, analisisservicios text, analisispuertosabiertos integer, analisisserviciosinseguros integer, analisisvulnerabilidades integer)")
+con.commit()
+con.close()
