82d2a6166a98f5f7a8c4be14aca02b043b145d30
AlejandroFdezGarcia <101207228+AlejandroFdezGarcia@users.noreply.github.com>
2023-03-24 14:35:35 +0100

    Crear BD alerts.csv

--- src/ejercicio3.py
+++ src/ejercicio3.py
@@ -1,1 +1,19 @@
 # EJERCICIO 3
+#Para crear en una base de datos los datos del archivo alerts.csv
+
+import sqlite3
+import csv
+
+conn = sqlite3.connect('practica1csv.db')
+
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE alertas
+                   (timestamp datetime, sid int, msg text, clasification text, priority int, protocol text, origin text, destination text, port int)''')
+
+with open('alerts.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        cursor.execute("INSERT INTO alertas (timestamp, sid, msg, clasification, priority, protocol, origin, destination, port) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
+
+conn.commit()
+conn.close()
