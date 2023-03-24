28d2c99c0066b08438a6f95f4614be5606dd3591
AlejandroFdezGarcia <101207228+AlejandroFdezGarcia@users.noreply.github.com>
2023-03-24 18:26:31 +0100

    Agrupar por fecha y alerta

--- src/ejercicio3.py
+++ src/ejercicio3.py
@@ -17,3 +17,30 @@ with open('alerts.csv', newline='') as csvfile:
 
 conn.commit()
 conn.close()
+
+#Para agrupar por prioridad de alerta y fecha
+conn = sqlite3.connect('pruebapractica1csv.db') 
+ 
+cursor = conn.cursor() 
+ 
+cursor.execute(''' 
+    SELECT 
+        CASE 
+            WHEN priority = 1 THEN 'Alertas graves' 
+            WHEN priority = 2 THEN 'Alertas medias' 
+            WHEN priority = 3 THEN 'Alertas bajas' 
+            ELSE 'Otras alertas' 
+        END AS prioridad, 
+        CASE 
+            WHEN timestamp BETWEEN '2022-07-01 00:00:00' AND '2022-07-31 23:59:59' THEN 'Julio' 
+            WHEN timestamp BETWEEN '2022-08-01 00:00:00' AND '2022-08-31 23:59:59' THEN 'Agosto' 
+            ELSE 'Otras fechas' 
+        END AS mes, 
+        COUNT(*) AS cantidad 
+    FROM alertas 
+    GROUP BY prioridad, mes; 
+''') 
+ 
+for row in cursor: 
+    print(row) 
+conn.close() 
