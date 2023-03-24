ce476fa782b5defff9e54feecbff08bbab475e5a
Carel Sánchez <52781415+Carel3DS@users.noreply.github.com>
2023-03-24 17:51:04 +0100

    Updated ejercicio2, added 'alertas' table

--- src/ejercicio2.py
+++ src/ejercicio2.py
@@ -1,9 +1,11 @@
 # EJERCICIO 2
 import sqlite3
+
 import pandas as pd
 import json
 
-con = sqlite3.connect('src/data/practica1.db')
+
+con = sqlite3.connect('data/practica1.db')
 cur = con.cursor()
 # cur.execute("CREATE TABLE maquinas("
 #             "id text,"
@@ -19,7 +21,7 @@ cur = con.cursor()
 # cur.execute("INSERT INTO maquinas VALUES ('web', '172.18.0.0', 'none', 'admin', 656445552, 'Administracion de sistemas', '[\"80/TCP\", \"443/TCP\", \"3306/TCP\", \"40000/UDP\"]', 3, 0, 15), ('paco_pc', '172.17.0.0', 'Barcelona', 'Paco Garcia', 640220120, 'Direccion', 'None', 0, 0, 4), ('luis_pc', '172.19.0.0', 'Madrid', 'Luis Sanchez', 'None', 'Desarrollador', '[\"1194/UDP\", \"8080/TCP\", \"8080/UDP\", \"40000/UDP\"]', 1, 1, 52), ('router1', '172.1.0.0', 'None', 'admin', 656445552, 'Administracion de sistemas', '[\"443/UDP\", \"80/TCP\"]', 1, 0, 3), ('dhcp_server', '172.1.0.1', 'Madrid', 'admin', 'None', 'None', '[\"80/TCP\", \"67/UDP\", \"68/UDP\"]', 2, 2, 12), ('mysql_db', '172.18.0.1', 'None', 'admin', 656445552, 'Administracion de sistemas', '[\"8080/TCP\", \"3306/TCP\", \"3306/UDP\"]', 2, 0, 2), ('ELK', '172.18.0.2', 'None', 'admin', 656445552, 'Administracion de sistemas', '[\"80/TCP\", \"443/TCP\", \"9200/TCP\", \"9300/TCP\", \"5601/TCP\"]', 3, 2, 21)")
 
 # Open the devices info as JSON and transform into SQL
-with open("src/data/devices.json") as f:
+with open("data/devices.json") as f:
     data = json.load(f)
     df = pd.DataFrame(data)
 
@@ -54,16 +56,20 @@ with open("src/data/devices.json") as f:
     usuarios = pd.concat(usuarios, axis=1).transpose()
 
     # Drop 'analisis' column, replace 'responsable' with 'nombres' array and merge with 'analisis' dataframe
-    maquinas = df.drop(columns='analisis').merge(analisis, on='id')
+    maquinas = df.drop(columns='analisis')
     maquinas['responsable'] = nombres
-    print(maquinas, "\n", ports)
 
     # Save ports and final df to database
-    ports.to_sql('puertos', con, if_exists='replace')
+    ports.to_sql('puertos', con, if_exists='replace')       # 'Replace' avoids failure when filling the table
     usuarios.to_sql('usuarios', con, if_exists='replace')
-    maquinas.to_sql('maquinas', con, if_exists='replace')  # 'Replace' avoids failure when filling the table
+    maquinas.to_sql('maquinas', con, if_exists='replace')
+    analisis.to_sql('analisis', con, if_exists='replace')
 
-# TEST final table
+# Extract transform CSV into alertas table
+with open("data/alerts.csv") as f:
+    pd.read_csv(f).to_sql('alertas', con, if_exists='replace')
+
+# TEST the database
 # cur.execute("SELECT * FROM usuarios JOIN maquinas ON maquinas.responsable=usuarios.nombre ")
 
 # Commit changes and close
@@ -75,3 +81,6 @@ con.close()
 ############
 
 # Use df, analisis, responsable dataframes to analyze data
+
+
+
