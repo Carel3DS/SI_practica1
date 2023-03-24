6fb189be024f58c601293903d0fb3a9e392fc53c
carel <carel3d@gmail.com>
2023-03-24 23:06:03 +0100

    ejercicio 2 finished

--- src/ejercicio2.py
+++ src/ejercicio2.py
@@ -11,7 +11,7 @@ import json
 ######################
 
 # Connect to the database
-con = sqlite3.connect('data/practica1.db')
+con = sqlite3.connect('src/data/practica1.db')
 cur = con.cursor()
 # cur.execute("CREATE TABLE maquinas("
 #             "id text,"
@@ -27,7 +27,7 @@ cur = con.cursor()
 # cur.execute("INSERT INTO maquinas VALUES ('web', '172.18.0.0', 'none', 'admin', 656445552, 'Administracion de sistemas', '[\"80/TCP\", \"443/TCP\", \"3306/TCP\", \"40000/UDP\"]', 3, 0, 15), ('paco_pc', '172.17.0.0', 'Barcelona', 'Paco Garcia', 640220120, 'Direccion', 'None', 0, 0, 4), ('luis_pc', '172.19.0.0', 'Madrid', 'Luis Sanchez', 'None', 'Desarrollador', '[\"1194/UDP\", \"8080/TCP\", \"8080/UDP\", \"40000/UDP\"]', 1, 1, 52), ('router1', '172.1.0.0', 'None', 'admin', 656445552, 'Administracion de sistemas', '[\"443/UDP\", \"80/TCP\"]', 1, 0, 3), ('dhcp_server', '172.1.0.1', 'Madrid', 'admin', 'None', 'None', '[\"80/TCP\", \"67/UDP\", \"68/UDP\"]', 2, 2, 12), ('mysql_db', '172.18.0.1', 'None', 'admin', 656445552, 'Administracion de sistemas', '[\"8080/TCP\", \"3306/TCP\", \"3306/UDP\"]', 2, 0, 2), ('ELK', '172.18.0.2', 'None', 'admin', 656445552, 'Administracion de sistemas', '[\"80/TCP\", \"443/TCP\", \"9200/TCP\", \"9300/TCP\", \"5601/TCP\"]', 3, 2, 21)")
 
 # Open the devices info as JSON and transform into SQL
-with open("data/devices.json") as f:
+with open("src/data/devices.json") as f:
     data = json.load(f)
     df = pd.DataFrame(data)
 
@@ -66,13 +66,13 @@ with open("data/devices.json") as f:
     maquinas['responsable'] = nombres
 
     # Save ports and final df to database
-    ports.to_sql('puertos', con, if_exists='replace')       # 'Replace' avoids failure when filling the table
+    ports.to_sql('puertos', con, if_exists='replace')  # 'Replace' avoids failure when filling the table
     usuarios.to_sql('usuarios', con, if_exists='replace')
     maquinas.to_sql('maquinas', con, if_exists='replace')
     analisis.to_sql('analisis', con, if_exists='replace')
 
 # Extract transform CSV into alertas table
-with open("data/alerts.csv") as f:
+with open("src/data/alerts.csv") as f:
     alertas = pd.read_csv(f)
     alertas.to_sql('alertas', con, if_exists='replace')
 
@@ -90,11 +90,11 @@ con.close()
 # Use all the dataframes we created to analyze data
 
 # Prepare Ports dataframe for analysis
-portGroup = ports[ports['puertos']!="None"].groupby('id').count()
+portGroup = ports[ports['puertos'] != "None"].groupby('id').count()
 
 # COUNTS
-devices = maquinas.__len__()    # Number of devices (1)
-alerts = alertas.__len__()      # Number of alerts (2)
+devices = maquinas.__len__()  # Number of devices (1)
+alerts = alertas.__len__()  # Number of alerts (2)
 
 # MEANS
 meanPorts = portGroup['puertos'].mean()
@@ -108,12 +108,24 @@ stdVulns = analisis['vulnerabilidades_detectadas'].std()
 
 # MINIMUMS
 minVulns = analisis['vulnerabilidades_detectadas'].min()
-minPorts = portGroup.min()
+minPorts = portGroup['puertos'].min()
 
 # MAXIMUMS
 maxVulns = analisis['vulnerabilidades_detectadas'].max()
-maxPorts = portGroup.max()
+maxPorts = portGroup['puertos'].max()
 
-print()
-
-
+print(
+    f"devices:\t{devices}",
+    f"alerts:\t\t{alerts}",
+    f"meanPorts:\t{meanPorts}",
+    f"meanInsec:\t{meanInsec}",
+    f"meanVulns:\t{meanVulns}",
+    f"stdPorts:\t{stdPorts}",
+    f"stdInsec:\t{stdInsec}",
+    f"stdVulns:\t{stdVulns}",
+    f"minVulns:\t{minVulns}",
+    f"minPorts:\t{minPorts}",
+    f"maxVulns:\t{maxVulns}",
+    f"maxPorts:\t{maxPorts}",
+    sep="\n"
+)
