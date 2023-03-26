d2ba652d9a2d48ee849fa5d78946389fd645b88d
AlejandroFdezGarcia <101207228+AlejandroFdezGarcia@users.noreply.github.com>
2023-03-26 13:00:13 +0200

    Seleccionar las IP más problemáticas

--- src/ejercicio4.py
+++ src/ejercicio4.py
@@ -1,1 +1,16 @@
 # EJERCICIO 4
+#Para seleccionar las IP de origen más problemáticas
+import sqlite3
+
+conn = sqlite3.connect('pruebapractica1csv.db')
+cursor = conn.cursor()
+
+query = "SELECT origin, COUNT(*) as count FROM alertas WHERE priority=1 GROUP BY origin ORDER BY count DESC LIMIT 10"
+cursor.execute(query)
+
+resultados = cursor.fetchall()
+
+for resultado in resultados:
+    print(resultado[0])
+
+conn.close()
