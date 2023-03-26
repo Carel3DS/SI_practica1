53ca264ab8a75c0a861aea18fd2c93ba7532737e
DanielSanchezCarrero <101207878+DanielSanchezCarrero@users.noreply.github.com>
2023-03-26 18:55:53 +0200

    Update ejercicio3.py

--- src/ejercicio3.py
+++ src/ejercicio3.py
@@ -19,10 +19,36 @@ conn.commit()
 conn.close()
 
 #Para agrupar por prioridad de alerta y fecha
-conn = sqlite3.connect('practica1csv.db') 
- 
-cursor = conn.cursor() 
- 
+con = sqlite3.connect("database.db")
+
+query = con.execute("SELECT * FROM alertas WHERE STRFTIME('%m',timestamp) = '07' AND PRIORITY = 1")
+cols = [column[0] for column in query.description]
+dAlertsJul1 = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
+
+query = con.execute("SELECT * FROM alertas WHERE STRFTIME('%m',timestamp) = '07' AND PRIORITY = 2")
+cols = [column[0] for column in query.description]
+dAlertsJul2 = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
+
+query = con.execute("SELECT * FROM alertas WHERE STRFTIME('%m',timestamp) = '07' AND PRIORITY = 3")
+cols = [column[0] for column in query.description]
+dAlertsJul3 = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
+
+query = con.execute("SELECT * FROM alertas WHERE STRFTIME('%m',timestamp) = '08' AND PRIORITY = 1")
+cols = [column[0] for column in query.description]
+dAlertsAgo1 = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
+
+query = con.execute("SELECT * FROM alertas WHERE STRFTIME('%m',timestamp) = '08' AND PRIORITY = 2")
+cols = [column[0] for column in query.description]
+dAlertsAgo2 = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
+
+query = con.execute("SELECT * FROM alertas WHERE STRFTIME('%m',timestamp) = '08' AND PRIORITY = 3")
+cols = [column[0] for column in query.description]
+dAlertsAgo3 = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
+
+
+cursor = con.cursor()
+
+#Número de observaciones y valores ausentes (mostrados como "Otras fechas" u "Otras alertas")
 cursor.execute(''' 
     SELECT 
         CASE 
@@ -39,8 +65,53 @@ cursor.execute('''
         COUNT(*) AS cantidad 
     FROM alertas 
     GROUP BY prioridad, mes; 
-''') 
- 
-for row in cursor: 
-    print(row) 
-conn.close() 
+''')
+
+for row in cursor:
+    print(row)
+
+#Mediana
+print("\n")
+mediana1 = (dAlertsJul1["sid"].count()+dAlertsAgo1["sid"].count()+1)/2
+mediana2 = (dAlertsJul2["sid"].count()+dAlertsAgo2["sid"].count()+1)/2
+mediana3 = (dAlertsJul3["sid"].count()+dAlertsAgo3["sid"].count()+1)/2
+medianaJulio = (dAlertsJul1["sid"].count()+dAlertsJul2["sid"].count()+dAlertsJul3["sid"].count()+1)/2
+medianaAgosto = (dAlertsAgo1["sid"].count()+dAlertsAgo2["sid"].count()+dAlertsAgo3["sid"].count()+1)/2
+medianaTot = (dAlertsJul1["sid"].count()+dAlertsJul2["sid"].count()+dAlertsJul3["sid"].count()+dAlertsAgo1["sid"].count()+dAlertsAgo2["sid"].count()+dAlertsAgo3["sid"].count()+1)/2
+
+print("La mediana de las alertas de prioridad 1 es la alerta número", mediana1)
+print("La mediana de las alertas de prioridad 2 es la alerta número", mediana2)
+print("La mediana de las alertas de prioridad 3 es la alerta número", mediana3)
+print("La mediana de las alertas del mes de julio es la alerta de número", medianaJulio)
+print("La mediana de las alertas del mes de agosto es la alerta de número", medianaAgosto)
+print("La mediana de todas las alertas es la alerta número", medianaTot)
+print("\n")
+
+#Media
+media1 = (dAlertsJul1["sid"].count()+dAlertsAgo1["sid"].count())/2
+media2 = (dAlertsJul2["sid"].count()+dAlertsAgo2["sid"].count())/2
+media3 = (dAlertsJul3["sid"].count()+dAlertsAgo3["sid"].count())/2
+mediaTot = (dAlertsJul1["sid"].count()+dAlertsJul2["sid"].count()+dAlertsJul3["sid"].count()+dAlertsAgo1["sid"].count()+dAlertsAgo2["sid"].count()+dAlertsAgo3["sid"].count())/2
+
+print("La media mensual de las alertas de prioridad 1 es de", media1, "alertas al mes")
+print("La media mensual de las alertas de prioridad 2 es de", media2, "alertas al mes")
+print("La media mensual de las alertas de prioridad 3 es de", media3, "alertas al mes")
+print("La media mensual de todas las alertas es de", mediaTot, "alertas al mes")
+print("\n")
+
+#Varianza
+varianza1 = ((dAlertsJul1["sid"].count()-media1)**2+(dAlertsAgo1["sid"].count()-media1)**2)/2
+varianza2 = ((dAlertsJul2["sid"].count()-media1)**2+(dAlertsAgo2["sid"].count()-media1)**2)/2
+varianza3 = ((dAlertsJul3["sid"].count()-media1)**2+(dAlertsAgo3["sid"].count()-media1)**2)/2
+varianzaTot = ((dAlertsJul1["sid"].count()+dAlertsJul2["sid"].count()+dAlertsJul3["sid"].count()-mediaTot)**2+(dAlertsAgo1["sid"].count()+dAlertsAgo2["sid"].count()+dAlertsAgo3["sid"].count()-mediaTot)**2)/2
+
+print("La varianza mensual de las alertas de prioridad 1 es de", varianza1, "alertas al cuadrado al mes")
+print("La varianza mensual de las alertas de prioridad 2 es de", varianza2, "alertas al cuadrado al mes")
+print("La varianza mensual de las alertas de prioridad 3 es de", varianza3, "alertas al cuadrado al mes")
+print("La varianza mensual de todas las alertas es de", varianzaTot, "alertas al mes")
+print("\n")
+
+#Valores Máximos y mínimos
+
+
+con.close()
